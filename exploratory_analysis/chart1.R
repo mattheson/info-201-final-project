28771168ee744a371d81b7fd0913574fd44d7a56
JyeDing <jacobye.7@gmail.com>
2021-11-18 16:39:47 -0800

    bar chart zoom

--- exploratory_analysis/chart1.R
+++ exploratory_analysis/chart1.R
@@ -1,7 +1,10 @@
-library(ggplot2)
-library(tidyverse)
+install.packages("ggforce")
+library("ggplot2")
+library("tidyverse")
+library("ggforce")
 data <- read.csv("~/info-201-final-project/data/Life Expectancy Data.csv")
 year <- aggregate(data[4],list(data$Year), mean, na.rm="TRUE")
 ggplot(year, aes(x=Group.1,y=Life.expectancy)) +
   geom_bar(stat="Identity", fill="steelblue") + 
-  xlab("Year") + ylab("Life Expectancy")
+  xlab("Year") + ylab("Life Expectancy") +
+  facet_zoom(ylim = c(60, 75))
