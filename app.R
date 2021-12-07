101297c1d58e98896e8a9fad1bcced1f6ef3e9c6
Matt Taniguchi <5974590+mattheson@users.noreply.github.com>
2021-12-06 20:32:30 -0800

    more boilerplate

--- app.R
+++ app.R
@@ -8,14 +8,19 @@ causes_data <- read.csv("./data/NCHS_-_Leading_Causes_of_Death__United_States.cs
 
 
 ui <- navbarPage("Title",
-  tabPanel("Introduction"),
-  tabPanel("Page 1"),
+  tabPanel("Introduction",
+           p("In this project, we were focused on analyzing ...")),
+  tabPanel("Page 1",),
   tabPanel("Page 2"),
   tabPanel("Page 3"),
-  tabPanel("Conclusion")
+  tabPanel("Conclusion",
+           p("From analyzing our results, we can see that ..."))
 )
 
 server <- function(input, output) {
+  # output$page1 <-
+  # output$page2 <-
+  # output$page3 <-
 }
 
 shinyApp(ui, server)
