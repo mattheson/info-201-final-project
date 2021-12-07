25b1a9af7c69533b8715a432ca0885593ee8e245
Matt Taniguchi <5974590+mattheson@users.noreply.github.com>
2021-12-07 14:28:10 -0800

    added first plot

--- app.R
+++ app.R
@@ -1,16 +1,30 @@
 library(shiny)
 library(tidyverse)
-library(readxl)
+# library(readxl)
 
 expectancy_data <- read.csv("./data/Life Expectancy Data.csv")
-mortality_data <- read_excel("./data/Mortality-rate-among-children-and-you-age-5-to-24_2020.xlsx")
+# mortality_data <- read_excel("./data/Mortality-rate-among-children-and-you-age-5-to-24_2020.xlsx")
 causes_data <- read.csv("./data/NCHS_-_Leading_Causes_of_Death__United_States.csv")
 
+# World plot
+mapdata <- map_data("world")
+world_plot_data <- rename(expectancy_data, "region" = "Country")
+mapdata_joined <- left_join(mapdata, world_plot_data, by="region")
+filter_mapdata <- mapdata_joined %>% filter(!is.na(mapdata_joined$Life.expectancy))
 
-ui <- navbarPage("Title",
+ui <- navbarPage("Info 201 Final Project",
   tabPanel("Introduction",
-           p("In this project, we were focused on analyzing ...")),
-  tabPanel("Page 1",),
+           h1("Analysis of Life Expectancy Worldwide"),
+           em(h3("Group _ (Ryan, Javid, Jacob, Matt")),
+           img(src="http://med.stanford.edu/content/dam/sm-news/images/2015/05/surgery-developing-world-msf.jpg", 
+               width="50%", height="50%", align="right"),
+           p("In this project, we were focused on analyzing data relating to mortality
+             and life expectancy from the U.S. and across the world. Our first dataset
+             is a Life Expectancy dataset from the WHO, containing data about life
+             expectancy across the world.")),
+  tabPanel("Page 1", 
+           sliderInput("year", label="Select year", min=2000, max=2015, value=2015, sep=""), 
+           plotOutput("worldplot")),
   tabPanel("Page 2"),
   tabPanel("Page 3"),
   tabPanel("Conclusion",
@@ -18,7 +32,18 @@ ui <- navbarPage("Title",
 )
 
 server <- function(input, output) {
-  # output$page1 <-
+  output$worldplot <- renderPlot(
+    ggplot(filter(filter_mapdata, Year==input$year),
+           aes(x=long, y=lat, group=group)) +
+    geom_polygon(aes(fill = Life.expectancy), color = "black") +
+    scale_fill_gradient(name="Average Life Expectancy", low="yellow", 
+                        high="blue", na.value="grey50") +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          rect = element_blank()))
   # output$page2 <-
   # output$page3 <-
 }
